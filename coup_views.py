7ffe82b4bbe0649e6b3586eb201e61c1f26a1464
Evan Xu <XuEvanJ@gmail.com>
2025-06-05 04:26:49 -0400

    Added barebones start game button and moved message embed code to coup_views.py

--- coup_views.py
+++ coup_views.py
@@ -2,19 +2,43 @@
 from discord.ui import Button, View
 import discord
 
-def create_join_game_button(players: dict, ctx, print_players):
+def create_lobby_view(players: dict, print_players, status, ctx):
+    # initialize a view
+    view = View()
+
+    view.add_item(join_bt(players, print_players, ctx))
+    view.add_item(leave_bt(players, print_players, ctx))
+    view.add_item(start_bt(players, status))
+
+    return view
+
+def create_lobby_embed(players: dict):
+    # convert display names into a string
+    players_string = "\n".join(players.values())
+
+    # create embed with title, description to view
+    embed = discord.Embed(
+        title = "Game of Coup!",
+        description = "2-6 Player Game",
+    )
+    # add player names to embed
+    embed.add_field(name="Players", value=players_string, inline=False)
+
+    return embed
+
+def join_bt(players: dict, print_players, ctx):
     button = Button(label="Join Game", style=discord.ButtonStyle.primary)
 
     async def callback(interaction: discord.Interaction):
         user = interaction.user
-        if user.id not in players:
+        if user.id not in players and len(players) < 6:
             players[user.id] = user.name
             await print_players(ctx)
     
     button.callback = callback
     return button
 
-def create_leave_game_button(players: dict, ctx, print_players):
+def leave_bt(players: dict, print_players, ctx):
     button = Button(label="Leave Game", style=discord.ButtonStyle.red)
 
     async def callback(interaction: discord.Interaction):
@@ -25,4 +49,16 @@ def create_leave_game_button(players: dict, ctx, print_players):
 
     button.callback = callback
     return button
+
+def start_bt(players: dict, status: bool):
+    button = Button(label="Start Game", style=discord.ButtonStyle.green)
+
+    async def callback(interaction: discord.Interaction):
+        user = interaction.user
+        if user.id in players:
+            status = True
+
+    button.callback = callback
+    return button
+
 
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 from discord.ui import Button, View
 
 # import commands from coup_views.py
-from coup_views import create_join_game_button, create_leave_game_button
+from coup_views import create_lobby_view, create_lobby_embed
 
 # Set up intents
 intents = discord.Intents.default()
@@ -18,11 +18,13 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print(f'Logged in as {bot.user}')  # This confirms the bot is logged in
 
-# Game of Coup
+# Coup Global Variables
 coup_status = False # A variable to track game's status
 players = {} # a variable to track game's players
 prev_msg = None
 
+
+# !coup command
 @bot.command(name="coup", help="Start a game of Coup")
 async def coup(ctx):
     # initialize variables as global
@@ -43,7 +45,7 @@ async def coup(ctx):
 
         await print_players(ctx)
 
-# Print Current Players for Game
+# !players command
 @bot.command(name="players", help="Print current players of Coup")
 async def show_players(ctx):
     await ctx.message.delete()
@@ -57,32 +59,14 @@ async def print_players(ctx):
     # declare global variables
     global prev_msg
 
-    # create a view
-    message = View()
-    # add buttons to the view
-    message.add_item(create_join_game_button(players, ctx, print_players))
-    message.add_item(create_leave_game_button(players, ctx, print_players))
-
-    # convert dict into string
-    players_string = "\n".join(players.values())
-    # add embed with title, description to view
-    embed = discord.Embed(
-        title = "Game of Coup!",
-        description = "2-6 Player Game",
-    )
-    # add player names to embed
-    embed.add_field(name="Players", value=players_string, inline=False)
+    view = create_lobby_view(players, print_players, coup_status, ctx)
+    embed = create_lobby_embed(players)
 
     # delete the previous lobby message
     if prev_msg != None:
         await prev_msg.delete()
 
     # output new message. It is now the previously sent message
-    prev_msg = await ctx.send(embed=embed, view=message)
-
-
-
-
-
+    prev_msg = await ctx.send(embed=embed, view=view)
        
 bot.run('MTMyODg4NTcyMjgxOTcyNzQ3Mg.G_1cyc.Ddxy1AjI2v1_OxYTGb7FJ7FSq_3jUc-hmVF7jM')
