7c6809f1615862a88a45f25a5a279ffa23371f13
Evan's Macbook <xuevanj@gmail.com>
2025-10-07 00:13:35 -0400

    feature(main): created loggers for the bot and the coup module

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import discord
 import os
 from discord.ext import commands
 from coup.controllers import setup as setup_coup
+from utils import setup_logger
 from dotenv import load_dotenv
 
 # load environment variables from .env file
@@ -19,10 +20,13 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 # Debug
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user}')  # This confirms the bot is logged in
+    # Create General bot logger
+    bot_logger = setup_logger("bot")
+    bot_logger.info(f'Logged in as {bot.user}')  # This confirms the bot is logged in
 
 async def main():
     # Load Coup cog
+    coup_logger = setup_logger("coup")
     await setup_coup(bot)
     await bot.start(os.getenv("DISCORD_BOT_TOKEN"))
 
