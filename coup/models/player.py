c964571359a4a102a9e898f7a1508d222e9650e8
Evan's Macbook <xuevanj@gmail.com>
2025-10-07 00:44:17 -0400

    Feature(player): Added logging for player.py

--- coup/models/player.py
+++ coup/models/player.py
@@ -1,9 +1,13 @@
 # player.py
 from typing import Optional
+import logging
+
+logger = logging.getLogger("coup")
 
 class Player():
     """Class represnting a player's state in the game."""
     def __init__(self, user_id: int, user_name: str):
+        logger.info(f"Created Player Object with UID: {user_id} and user_name: {user_name}")
         self.user_id = user_id
         self.user_name = user_name
         self.coins = 2
@@ -22,6 +26,7 @@ class Player():
 
     def lose_influence(self, card: Optional[str]) -> str:
         """Handles the player losing an influence (card)"""
+        logger.info(f"Player {self.user_id} is losing an influence. Specified: {card}")
         if len(self.hand) == 2:
             if card in self.hand:
                 return self.hand.remove(card)
@@ -31,23 +36,26 @@ class Player():
         elif len(self.hand) == 1:
             return self.hand.pop()
         else:
-            raise ValueError("Player is already out of the game.")
+            logger.error(f"Player {self.user_id} is already dead, cannot lose influence.")
     
     def gain_influence(self, card: str):
         """Adds a card to the player's hand (user exchanges or challenge win)"""
+        logger.info(f"Player {self.user_id} is gaining influence: {card}")
         if len(self.hand) >= 2:
-            raise ValueError("Player cannot have more than 2 influence.")
+            logger.error(f"Player {self.user_id} cannot have more than 2 influence.")
         self.is_alive = True # in case player was at 0 cards before gaining influence
         self.hand.append(card)
         
     def gain_income(self, amount: int):
         """Increase player's coins by the specified amount"""
         self.coins += amount
+        logger.info(f"Player {self.user_id} gained {amount} coins. Now owns {self.coins} coins.")
     
     def spend_coins(self, amount: int):
         """Decrease player's coins by the specified amount"""
         if amount > self.coins:
-            raise ValueError("Not enough coins to take this action.")
+            logger.error("Not enough coins to take this action.")
         self.coins -= amount
+        logger.info(f"Player {self.user_id} spent {amount} coins. Now owns {self.coins} coins.")
 
     
