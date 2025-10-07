7fc8aa67dc33f9e2d63d8100db37309fd75660e3
Evan's Macbook <xuevanj@gmail.com>
2025-10-07 07:01:01 -0400

    refactor(action): Refactor action into inheritable class. Subclasses implement specific actions.

--- coup/models/action.py
+++ coup/models/action.py
@@ -1,28 +1,85 @@
 # action.py
-class Action:
-    turnActions = ["income", "foreign_aid", "coup", "tax", "steal", "assassinate", "exchange"]
-    roleActions = ["tax", "steal", "assassinate", "exchange"]
-    roleDict = {"tax": "Duke", 
-                "steal": "Captain",
-                "assassinate": "Assassin",
-                "exchange": "Ambassador",
-                }
-    
-    def __init__(self):
-        self.action = None # Current Action
-        # Info Regarding an Action being blocked
-        self.blocked = False
-        self.blocker_id = None
-        self.blocker_name = None
-        self.blocking_role = None
-        # Info Regarding a role action being challenged
-        self.challenged = False
-        self.challenger_id = None
-        self.challenger_name = None
-        # Info Regarding Action Target
-        self.target_id = None
-        self.target_name = None
+from abc import ABC
+from typing import Optional
+from .player import Player
+import logging
 
-    def getRole(self, action):
-        """Takes an action and returns the associated role"""
-        return self.roleDict[action]
+logger = logging.getLogger("coup")
+
+class Action(ABC):
+    """Base class for all game actions"""
+    name: str = "base"
+
+    def __init__(self, actor: Player, target: Optional[Player] = None):
+        self.actor = actor
+        self.target = target
+        self.blocker: Optional[Player]
+        self.challenger: Optional[Player]
+        self.blocked = False
+        self.challenged = False
+
+    async def execute(self, game):
+        logger.error("Base Class Method Called")
+        
+    async def on_block(self, game, blocker):
+        pass
+
+class Income(Action):
+    name = "income"
+    async def execute(self, game):
+        self.actor.gain_income(1)
+        await game.send_update_msg(
+            content=f"{self.actor.user_name} gained 1 coin. They now have {self.actor.coins} coin(s)."
+        )
+        await game.end_turn()
+
+class Foreign_Aid(Action):
+    name = "foreign_aid"
+    async def execute(self, game):
+        self.actor.gain_income(2)
+        await game.send_update_msg(
+            content=f"{self.actor.user_name} gained 2 coins. They now have {self.actor.coins} coin(s)."
+        )
+        await game.end_turn()
+
+class Tax(Action):
+    name = "tax"
+    async def execute(self, game):
+        self.actor.gain_income(3)
+        await game.send_update_msg(
+            content=f"{self.actor.user_name} gained 3 coins. They now have {self.actor.coins} coin(s)."
+        )
+        await game.end_turn()
+
+class Coup(Action):
+    name = "coup"
+    async def execute(self, game):
+        """COUP MUST BE AFFORDABLE. HANDLE IN ACTION_VIEW"""
+        game.deck.return_revealed(self.target.lose_influence()) # Target Loses Influence
+        self.actor.spend_coins(7) # Actor spends 7 coins
+        await game.end_turn()
+
+class Exchange(Action):
+    name = "exchange"
+    async def execute(self, game):
+        game.deck.return_deck(self.actor.lose_influence()) # Lose actor's choice of influence
+        self.actor.gain_influence(game.deck.draw()) # Draw a new role card
+
+class Assasinate(Action):
+    name = "assassinate"
+    async def execute(self, game):
+        game.deck.return_revealed(self.target.lose_influence()) # Target Loses Influence
+        self.actor.spend_coins(3) # Actor spends 3 coins
+        await game.end_turn()
+
+class Steal(Action):
+    name = "steal"
+    async def execute(self, game):
+        self.target.lose_coins(2) # Target loses 2 coins
+        self.actor.gain_income(2) # Actor gains 2 coins
+        await game.end_turn()
+
+
+
+
+        
