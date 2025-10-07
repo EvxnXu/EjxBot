80535886d4a027c8ff45edf5f011f32a036bf49a
Evan's Macbook <xuevanj@gmail.com>
2025-10-07 00:10:21 -0400

    feature(logger): add setup_logger routine

---
+++ utils/logger.py
@@ -0,0 +1,26 @@
+# logger.py
+import logging
+import os
+from logging.handlers import TimedRotatingFileHandler
+
+def setup_logger(name):
+    """Sets up a daily rotating logger that saves to logs/YYYY-MM-DD.log"""
+    log_dir = "logs"
+    os.makedirs(log_dir, exist_ok=True)
+
+    log_path = os.path.join(log_dir, "game.log")
+
+    handler = TimedRotatingFileHandler(
+        log_path, when="midnight", interval=1, backupCount=7, encoding="utf-8"
+    )
+    handler.suffix="%Y-%m-%d"
+
+    formatter = logging.Formatter(
+        "%(asctime)s [%(levelname)s] %(name)s: %(messages)s"
+    )
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    return logger
