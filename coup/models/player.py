7714bcac1fe43813d6cd320ba7b7aa5a26dab480
Evan's Macbook <xuevanj@gmail.com>
2025-10-07 01:03:22 -0400

    refactor(player): Added __repr__ and updated logger messages

--- coup/models/player.py
+++ coup/models/player.py
@@ -7,55 +7,61 @@ logger = logging.getLogger("coup")
 class Player():
     """Class represnting a player's state in the game."""
     def __init__(self, user_id: int, user_name: str):
+
         logger.info(f"Created Player Object with UID: {user_id} and user_name: {user_name}")
         self.user_id = user_id
         self.user_name = user_name
         self.coins = 2
         self.hand = []
+
+    def __repr__(self):
+        return f"<Player {self.user_name} ({self.user_id}) coins={self.coins} hand={self.hand}>"
     
     def is_alive(self) -> bool:
-        if len(self.hand) == 0:
-            return False
-        return True
+        """Checks if Player still has an influence card."""
+        return len(self.hand) > 0
     
     def check_role(self, role: str) -> bool:
         """Checks if Player has the passed role."""
-        if role in self.hand:
-            return True
-        return False
+        return role in self.hand
 
     def lose_influence(self, card: Optional[str]) -> str:
         """Handles the player losing an influence (card)"""
-        logger.info(f"Player {self.user_id} is losing an influence. Specified: {card}")
+        if not self.is_alive():
+            logger.error(f"Player {self} is already dead; cannot lose influence.")
+            return None
+        
         if len(self.hand) == 2:
-            if card in self.hand:
-                return self.hand.remove(card)
+            if card and card in self.hand:
+                self.hand.remove(card)
+                logger.info(f"Player {self} lost influence: {card}")
+                return card
             else:
                 print("Player has 2 cards, needs to choose one to lose.")
                 # TODO: Implement logic for providing the user a message + view with buttons to choose card
         elif len(self.hand) == 1:
-            return self.hand.pop()
-        else:
-            logger.error(f"Player {self.user_id} is already dead, cannot lose influence.")
+            lost_card = self.hand.pop()
+            logger.info(f"Player {self} lost their last influence: {lost_card}")
+            return lost_card
+
     
     def gain_influence(self, card: str):
         """Adds a card to the player's hand (user exchanges or challenge win)"""
-        logger.info(f"Player {self.user_id} is gaining influence: {card}")
         if len(self.hand) >= 2:
-            logger.error(f"Player {self.user_id} cannot have more than 2 influence.")
-        self.is_alive = True # in case player was at 0 cards before gaining influence
+            logger.error(f"Player {self} cannot have more than 2 influence.")
         self.hand.append(card)
+        logger.info(f"Player {self} gained influence: {card}")
         
     def gain_income(self, amount: int):
         """Increase player's coins by the specified amount"""
         self.coins += amount
-        logger.info(f"Player {self.user_id} gained {amount} coins. Now owns {self.coins} coins.")
+        logger.info(f"Player {self} gained {amount} coin(s)")
     
     def spend_coins(self, amount: int):
         """Decrease player's coins by the specified amount"""
         if amount > self.coins:
-            logger.error("Not enough coins to take this action.")
+            logger.error(f"Player {self} does not have enough coins to spend {amount}")
+            return
         self.coins -= amount
-        logger.info(f"Player {self.user_id} spent {amount} coins. Now owns {self.coins} coins.")
-
+        logger.info(f"Player {self} spent {amount} coin(s)")
     
