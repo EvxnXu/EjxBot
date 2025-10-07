559b70586880b2b63a4e6bed42db60bc69d56a09
Evan's Macbook <xuevanj@gmail.com>
2025-10-07 00:38:17 -0400

    feature(logger): outputs log messages to stdout as well now

--- utils/logger.py
+++ utils/logger.py
@@ -1,6 +1,7 @@
 # logger.py
 import logging
 import os
+import sys
 from logging.handlers import TimedRotatingFileHandler
 
 def setup_logger(name):
@@ -8,22 +9,27 @@ def setup_logger(name):
     log_dir = "logs"
     os.makedirs(log_dir, exist_ok=True)
 
-    if name == "coup":
-        log_path = os.path.join(log_dir, "coup.log")
-    else:
-        log_path = os.path.join(log_dir, "bot.log")
+    # Name log file depending on name
+    log_path = os.path.join(log_dir, f"{name}.log")
 
+    # File handler (rotates daily, keeps 7 days)
     handler = TimedRotatingFileHandler(
         log_path, when="midnight", interval=1, backupCount=7, encoding="utf-8"
     )
     handler.suffix="%Y-%m-%d"
 
+    # Console handler
+    console_handler = logging.StreamHandler(sys.stdout)
+
+    # Formatter
     formatter = logging.Formatter(
         "%(asctime)s [%(levelname)s] %(name)s: %(message)s"
     )
     handler.setFormatter(formatter)
 
+    # Logger
     logger = logging.getLogger(name)
+    logger.addHandler(console_handler)
     logger.addHandler(handler)
     logger.propagate = False
     logger.setLevel(logging.INFO)
