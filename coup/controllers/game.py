bd9282cd445f68a1f35989bb35273218bd39b6a3
Evan's Macbook <xuevanj@gmail.com>
2025-10-21 14:18:39 -0400

    feat: send update message about card swap for failed challenges

--- coup/controllers/game.py
+++ coup/controllers/game.py
@@ -272,6 +272,9 @@ class Game:
                 # Blocker Exchanges Challenged Role
                 await self.handle_lose_influence(player=blocker, card=action.blocking_role, exchange=True)
                 blocker.gain_influence(self.deck.draw())
+                await self.send_update_msg(
+                    f"{blocker.name} is exchanging {action.blocking_role} with a new card from the deck."
+                )
                 # Check if Challenger is Alive
                 await self.check_alive(challenger)
                 # Action is Blocked
@@ -309,6 +312,9 @@ class Game:
                 # Actor Exchanges Challenged Role
                 await self.handle_lose_influence(player=actor, card=acting_role, exchange=True)
                 actor.gain_influence(self.deck.draw())
+                await self.send_update_msg(
+                    f"{actor.name} is exchanging {acting_role} with a new card from the deck."
+                )
                 # Check if Challenger is Alive
                 await self.check_alive(challenger)
                 # Carry out Action
